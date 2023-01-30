b7563d88835fff84e9e079bcc62017ca11840793
codedit334 <oussama7elabdioui@gmail.com>
2023-01-30 14:52:48 +0100

    Fix linter errors

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const ham = document.querySelector('.nav-btn-ham');
 const x = document.querySelector('.nav-btn-x');
 const navMenu = document.querySelector('.nav-menu');
 
-ham.addEventListener('click',()=>{
-    navMenu.classList.toggle("active");
-})
-x.addEventListener('click',()=>{
-    navMenu.classList.toggle("active");
-})
-document.querySelectorAll('.nav-menu-list').forEach(n=>n.addEventListener('click',()=>{
-    navMenu.classList.toggle("active");
-}))
+ham.addEventListener('click', () => {
+  navMenu.classList.toggle('active');
+});
+x.addEventListener('click', () => {
+  navMenu.classList.toggle('active');
+});
+document.querySelectorAll('.nav-menu-list').forEach((n) => n.addEventListener('click', () => {
+  navMenu.classList.toggle('active');
+}));
